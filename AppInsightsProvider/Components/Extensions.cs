9f70a8653943516354e803085880f3e5dc846dc9
David Rodriguez <davidj@intelequia.com>
2019-09-12 10:33:22 +0100

    Error logging exception (Issue #14)

--- AppInsightsProvider/Components/Extensions.cs
+++ AppInsightsProvider/Components/Extensions.cs
@@ -11,7 +11,17 @@ namespace DotNetNuke.Monitoring.AppInsights.Components
         public static void AddIfNotNull(this Dictionary<string, string> dictionary, string key, string value)
         {
             if (!string.IsNullOrEmpty(value))
-                dictionary.Add(key, value);
+            {
+                if (dictionary.ContainsKey(key))
+                {
+                    dictionary[key] = value;
+                }
+                else
+                {
+                    dictionary.Add(key, value);
+                }                
+            }
+                
         }
     }
 }
